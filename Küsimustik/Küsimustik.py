2bd1618978b0702d1573336160f2112b8dbe9cb4
opilane <opilane@HP-CZC2349HTM>
2024-03-18 11:37:48 +0200

    Küsimustik

--- Küsimustik/Küsimustik.py
+++ Küsimustik/Küsimustik.py
@@ -1,8 +1,38 @@
 from MinuOmaMoodul import *
-kus_vas={}
+from random import *
 
-küsimused,vastused=loe_ankeet("Ankeet.txt")
-print(küsimused)
-print(vastused)
-for i in range(len(küsimused)):
-    print("Küsimus on: "+küsimused[i]+", vastus on:"+vastused[i])
+file=open("Ankeet.txt", "r",encoding="utf-8")
+kasutajanimed=[]
+salasõnad=[]
+while True:
+    print("1-Administraatori registreerimine\n2-Kasutaja\n3-Autoriseerimine")
+    vastus=int(input("Sisestage arv "))
+    if vastus==1:
+        print("Administraatori registreerimine")
+        kasutajanimed,salasõnad=registreerimine(kasutajanimed,salasõnad)
+    elif vastus==2:
+        print("Kasutaja")
+        print("Tere tulemast viktoriinile!")
+        nimi=input("Mis on sinu nimi? ")
+        print("Tere", nimi)
+        õigevastus=0
+        for rida in file:
+            N=rida.split(":")
+            print(N[0])
+            katsed=3
+            while katsed>0:
+                sisend=input("Vastus: ").strip()
+                if sisend==N[1].strip():
+                    õigevastus+=1
+                    print("Tubli!")
+                else:
+                    katsed-=1
+                    print("Viga. Teil on",katsed,"katse.")
+            else:
+                print("Enam ktseid pole jäänud. Õige vastus on:",N[1].strip())
+        print("Õigete vastuste arv:",õigevastus)
+    elif vastus==3:
+        autoriseerimine(kasutajanimed,salasõnad)
+        küsimused=loe_ankeet("Ankeet.txt")
+        print(f"Küsimused on: "+str(küsimused[0]),"Vastused on: "+str(küsimused[1]))
+        file.close()
--- Küsimustik/MinuOmaMoodul.py
+++ Küsimustik/MinuOmaMoodul.py
@@ -1,3 +1,7 @@
+from string import *
+from time import sleep
+from os import path, remove
+
 def loe_ankeet(fail:str)->any:
     fail=open(fail,"r",encoding="utf-8")
     kus=[]
@@ -13,3 +17,89 @@ def loe_ankeet(fail:str)->any:
 
     fail.close()
     return kus,vas #,kus_vas
+
+def lisa_kusimus(failinimi, kusimus, vastus):
+    fail=open(failinimi, 'a', encoding='utf-8')
+    fail.write(f"{kusimus}:{vastus}\n")
+    fail.close()
+
+def testi_osalejat(kusimused_vastused, nimi):
+    print(f"Tere, {nimi}! Alustame testi.")
+    kusimused=list(kusimused_vastused.keys())
+    
+    õiged_vastused=0
+    for kusimus in kusimused:
+        print(f"{nimi}, küsimus: {kusimus}")
+        vastus=input("Sinu vastus: ")
+        if vastus.lower()==kusimused_vastused[kusimus].lower():
+            õiged_vastused+=1
+    return õiged_vastused
+
+from string import *
+from time import sleep
+from os import path, remove
+def registreerimine(kasutajad:list,paroolid:list)->any:
+    """Funktsioon tagastab kasutajad ja paroolid listid:
+    :param list kasutajad:Kasutaja peab sisestama kasutajanime ja see tuleks nimekirja lisada
+    :param list paroolid:Kasutaja peab sisestama parooli ja see parool tuleb nimekirja lisada
+    :rtype:list,list
+    """
+    while True:
+        nimi=input("Mis on sinu nimi? ")
+        if nimi not in kasutajad:
+            while True:
+                parool=input("Mis on sinu parool? ")
+                flag_p=False
+                flag_l=False
+                flag_u=False
+                flag_d=False
+                if len(parool)>=8:
+                    parool_list=list(parool)
+                    for p in parool_list:
+                        if p in punctuation:
+                            flag_p=True
+                        elif p in ascii_lowercase:
+                            flag_l=True
+                        elif p in ascii_uppercase:
+                            flag_u=True
+                        elif p in digits:
+                            flag_d=True
+                    if flag_p and flag_u and flag_l and flag_d:
+                        kasutajad.append(nimi)
+                        paroolid.append(parool)
+                    break
+                else:
+                    print("Nõrk salasõna!")
+            break
+        else:
+            print("Selline kasutaja on juba olemas!")
+    return kasutajad, paroolid
+def autoriseerimine(kasutajad:list,paroolid:list):
+    """Funktsioon kuvab ekraanile "Tere tulemast!" kui kasutaja on olemas nimekirjas
+         Nimi on järjendis kasutajad
+         Salasõna on paroolide järjendis
+         Nimi ja salasõna indeksid on võrdsed
+    :param list kasutajad:...
+    :param list paroolid:...
+    """
+    p=0
+    while True:
+        nimi=input("Sisesta kasutajanimi: ")
+        if nimi in kasutajad:
+            while True:
+                parool=input("Sisesta salasõna: ")
+                p+=1
+                try:
+                    if kasutajad.index(nimi)==paroolid.index(parool):
+                        print (f"Tere tulemast! {nimi}")
+                        break
+                except:
+                    print("Vale nimi või salasõna!")
+                    if p==5:
+                        print("Proovi uuesti 10 sek pärast")
+                        for i in range(10):
+                            sleep(1)
+                            print(f"On jäänud {10-i} sek")
+        else:
+            print("Kasutajat pole")
+        break
