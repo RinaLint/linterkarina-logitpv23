31a626e234dd160488f2eb79f10feaa6b3295c67
opilane <opilane@HP-CZC2349HTM>
2024-04-08 11:36:46 +0300

    tkinteri

--- Tkinteri_kasutamine/Tkinteri_kasutamine.py
+++ Tkinteri_kasutamine/Tkinteri_kasutamine.py
@@ -4,37 +4,39 @@ def tehtudvalik(var):
     f=var.get()
     if f:
         texbox.configure(show="")
+        valik.configure(image=pilt1)
     else:
         texbox.configure(show="*")
+        valik.configure(image=pilt2)
 def textpealkirjasse():
     t=texbox.get()
     pealkiri.configure(text=t)
     texbox.delete(0,END)
-
 aken=Tk()
 aken.geometry("500x500")
 aken.title("Akna pealkiri")
-aken.configure(bg="#ffc9fd")
+aken.configure(bg="#3deff5")
 aken.iconbitmap("icon.ico")
 pealkiri=Label(aken,
                text="Põhielemendid",
-               bg="#86e2eb",
+               bg="#df7ef7",
                fg="#000000",
-               cursor="heart",
-               font="Elephant 16",
+               cursor="sizing",
+               font="Goudy_Old_Style 16",
                justify=CENTER,
                height=3,width=26)
 raam=Frame(aken)
 texbox=Entry(raam,
               bg="#000000",
-              fg="#86e2eb",
-              font="Elephant 16",
+              fg="#df7ef7",
+              font="Goudy_Old_Style 16",
               width=16,
               show="*")
-pilt=PhotoImage(file="eye.png")
+pilt1=PhotoImage(file="eye.png")
+pilt2=PhotoImage(file="eye.png")
 var=BooleanVar() #IntVar(), StringVar()
 valik=Checkbutton(raam,
-                  image=pilt,
+                  image=pilt2, #text="Punkt1"
                   variable=var,
                   onvalue=True,
                   offvalue=False,
@@ -44,13 +46,13 @@ nupp=Button(raam,
             text="Vajutaja mind",
             bg="#000000",
             fg="#86e2eb",
-            font="Elephant 16",
+            font="Goudy_Old_Style 16",
             width=16,
             command=textpealkirjasse)
 
 pealkiri.pack()
 raam.pack()
-texbox.grid(row=0,column=0)
-valik.grid(row=0,column=1)
-nupp.grid(row=0,column=2)
+texbox.grid(row=0,column=0) #raami seed
+valik.grid(row=0,column=1) #raami seed
+nupp.grid(row=0,column=2) #raami seed
 aken.mainloop()
