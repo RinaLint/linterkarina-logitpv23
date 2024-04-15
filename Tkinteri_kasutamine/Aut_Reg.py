dceab8e8792b197dcbb1c83ee2ce700e3c3b8679
opilane <opilane@HP-CZC2349HTM>
2024-04-15 11:38:41 +0300

    tkinteri

--- Tkinteri_kasutamine/Aut_Reg.py
+++ Tkinteri_kasutamine/Aut_Reg.py
@@ -4,11 +4,11 @@ from tkinter import messagebox as mb
 def tehtudvalik(var, nimi, text):
     f=var.get()
     if f:
-        texbox.configure(show="")
-        valik.configure(image=pilt1)
+        text.configure(show="")
+        nimi.configure(image=pilt1)
     else:
-        texbox.configure(show="*")
-        valik.configure(image=pilt2)
+        text.configure(show="*")
+        nimi.configure(image=pilt2)
 def aut():
     kasutajanimi=kasutaja.get()
     parool=texbox.get() 
@@ -24,8 +24,19 @@ def reg():
         mb.showinfo("Registreerimine", f"Kasutaja {kasutajanimi2} registreeritud!")
     else:
         mb.showerror("Registreerimine", "Kasutajanimi on juba registreeritud!")
+def muuda():
+    vana_kasutajanimi=kasutaja_muuda.get()
+    vana_parool=texbox_muuda.get()
+    uus_kasutajanimi=uus_kasutaja.get()
+    uus_parool=uus_texbox.get()
+    kasutaja_info=(vana_kasutajanimi, vana_parool)
+    if kasutaja_info in kasutajad:
+        kasutaja.kasutajakasutaja.append((uus_kasutajanimi, uus_parool))
+        mb.showinfo("Muuda nime ja paoolit," "Kasutaja andmed edukalt muudetud!")
+    else:
+        mb.showerror("Muuda nime ja paoolit", "Sellist kasutaja ei leitud!")
 aken=Tk()
-aken.geometry("700x700")
+aken.geometry("800x800")
 aken.title("Autoriseerimine või registreerimine")
 aken.configure(bg="#3deff5")
 aken.iconbitmap("icon.ico")
@@ -41,12 +52,12 @@ pealkiri=Label(aken,
 raam=Frame(aken)
 kasutaja=Entry(raam,
               bg="#f5a3a2",
-              fg="#df7ef7",
+              fg="#000000",
               font="Goudy_Old_Style 16",
               width=16)
 texbox=Entry(raam,
               bg="#f5a3a2",
-              fg="#df7ef7",
+              fg="#000000",
               font="Goudy_Old_Style 16",
               width=16,
               show="*")
@@ -54,7 +65,7 @@ pilt1=PhotoImage(file="eye.png")
 pilt2=PhotoImage(file="show.png")
 var=BooleanVar()
 valik=Checkbutton(raam,
-                  image=pilt2,
+                  image=pilt1,
                   variable=var,
                   onvalue=True,
                   offvalue=False,
@@ -62,7 +73,7 @@ valik=Checkbutton(raam,
 nupp=Button(raam,
             text="Sisenema",
             bg="#f5a3a2",
-            fg="#86e2eb",
+            fg="#000000",
             font="Goudy_Old_Style 16",
             width=16,
             command=aut)
@@ -77,31 +88,66 @@ pealkiri2=Label(aken,
 raam2=Frame(aken)
 kasutaja2=Entry(raam2,
               bg="#f5a3a2",
-              fg="#df7ef7",
+              fg="#000000",
               font="Goudy_Old_Style 16",
               width=16)
 texbox2=Entry(raam2,
               bg="#f5a3a2",
-              fg="#df7ef7",
+              fg="#000000",
               font="Goudy_Old_Style 16",
               width=16,
               show="*")
-pilt1=PhotoImage(file="eye.png")
-pilt2=PhotoImage(file="show.png")
-var=BooleanVar()
 valik2=Checkbutton(raam2,
-                  image=pilt2,
+                  image=pilt1,
                   variable=var,
                   onvalue=True,
                   offvalue=False,
                   command=lambda:tehtudvalik(var,valik2,texbox2))
 nupp2=Button(raam2,
             text="Registreerima",
-            bg="#000000",
-            fg="#86e2eb",
+            bg="#f5a3a2",
+            fg="#000000",
             font="Goudy_Old_Style 16",
             width=16,
             command=reg)
+pealkiri3=Label(aken,
+               text="Muuda nime ja paroolit",
+               bg="#df7ef7",
+               fg="#000000",
+               cursor="sizing",
+               font="Goudy_Old_Style 16",
+               justify=CENTER,
+               height=3,width=26)
+raam3=Frame(aken)
+kasutaja_muuda=Entry(raam3,
+              bg="#f5a3a2",
+              fg="#000000",
+              font="Goudy_Old_Style 16",
+              width=16)
+texbox_muuda=Entry(raam3,
+              bg="#f5a3a2",
+              fg="#000000",
+              font="Goudy_Old_Style 16",
+              width=16,
+              show="*")
+uus_kasutaja=Entry(raam3,
+              bg="#f5a3a2",
+              fg="#000000",
+              font="Goudy_Old_Style 16",
+              width=16)
+uus_texbox=Entry(raam3,
+              bg="#f5a3a2",
+              fg="#000000",
+              font="Goudy_Old_Style 16",
+              width=16,
+              show="*")
+nupp_muuda=Button(raam3,
+            text="Muuda nime ja parool",
+            bg="#f5a3a2",
+            fg="#000000",
+            font="Goudy_Old_Style 16",
+            width=16,
+            command=muuda)
 pealkiri.pack()
 raam.pack()
 kasutaja.grid(row=0, column=0)
@@ -114,4 +160,11 @@ kasutaja2.grid(row=0, column=0)
 texbox2.grid(row=0, column=1)
 valik2.grid(row=0, column=2)
 nupp2.grid(row=0, column=3)
+pealkiri3.pack()
+raam3.pack()
+kasutaja_muuda.grid(row=0, column=0)
+texbox_muuda.grid(row=0, column=1)
+uus_kasutaja.grid(row=1, column=0)
+uus_texbox.grid(row=1, column=1)
+nupp_muuda.grid(row=2, columnspan=2)
 aken.mainloop()
