374e1f75aa3e8ccc230124a540d2050e24ed35c9
opilane <opilane@HP-CZC2349HTM>
2024-04-16 11:40:03 +0300

    tkinteri

--- Tkinteri_kasutamine/Aut_Reg.py
+++ Tkinteri_kasutamine/Aut_Reg.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import smtplib, ssl
+from email.message import EmailMessage
+import imghdr
 
 def tehtudvalik(var, nimi, text):
     f=var.get()
@@ -17,6 +21,9 @@ def aut():
     else:
         mb.showerror("Autentimine", "Kasutajanimi ei ole registreeritud!")
 def reg():
+    mail=sd.askstring("Kirjuta oma e-posti!", "Kuhu saada kirja?")
+    password=sd.askstring("Kirjuta om parool!", "Kirjuta oma salasõna")
+    email(mail, password)
     kasutajanimi2=kasutaja2.get()
     parool2=texbox2.get()
     if (kasutajanimi2,parool2) not in kasutajad:
@@ -31,10 +38,29 @@ def muuda():
     uus_parool=uus_texbox.get()
     kasutaja_info=(vana_kasutajanimi, vana_parool)
     if kasutaja_info in kasutajad:
-        kasutaja.kasutajakasutaja.append((uus_kasutajanimi, uus_parool))
-        mb.showinfo("Muuda nime ja paoolit," "Kasutaja andmed edukalt muudetud!")
+        kasutajad.remove(kasutaja_info)
+        kasutajad.append((uus_kasutajanimi, uus_parool))
+        mb.showinfo("Muuda nime ja parolit", "Kasutaja andmed edukalt muudetud!")
     else:
-        mb.showerror("Muuda nime ja paoolit", "Sellist kasutaja ei leitud!")
+        mb.showerror("Muuda nime ja parolit", "Sellist kasutaja ei leitud!")
+def email(mail:str, password:str):
+    smtp_server="smtp.gmail.com"
+    port=587
+    sender_email="karinalinter7@gmail.com"
+    password=password
+    context=ssl.create_default_context()
+    msg=EmailMessage()
+    msg.set_content("")
+    msg['Subject']="Olete edukalt registreeritud!"
+    msg['From']="Karina Linter" 
+    msg['To']=mail
+    try:
+        server=smtplib.SMTP(smtp_server,port)
+        server.starttls(context=context)
+        server.login(sender_email,password)
+        server.send_message(msg)
+    finally:
+        server.quit()
 aken=Tk()
 aken.geometry("800x800")
 aken.title("Autoriseerimine või registreerimine")
@@ -148,6 +174,24 @@ nupp_muuda=Button(raam3,
             font="Goudy_Old_Style 16",
             width=16,
             command=muuda)
+pealkiri4=Label(raam3,
+               text="Vanad andmed",
+               bg="#f3f576",
+               fg="#000000",
+               cursor="sizing",
+               font="Goudy_Old_Style 16",
+               justify=CENTER,
+               height=2,width=16)
+raam4=Frame(aken)
+pealkiri5=Label(raam3,
+               text="Uued andmed",
+               bg="#f3f576",
+               fg="#000000",
+               cursor="sizing",
+               font="Goudy_Old_Style 16",
+               justify=CENTER,
+               height=2,width=16)
+raam5=Frame(aken)
 pealkiri.pack()
 raam.pack()
 kasutaja.grid(row=0, column=0)
@@ -161,10 +205,14 @@ texbox2.grid(row=0, column=1)
 valik2.grid(row=0, column=2)
 nupp2.grid(row=0, column=3)
 pealkiri3.pack()
+raam4.pack()
+raam5.pack()
 raam3.pack()
-kasutaja_muuda.grid(row=0, column=0)
-texbox_muuda.grid(row=0, column=1)
-uus_kasutaja.grid(row=1, column=0)
-uus_texbox.grid(row=1, column=1)
-nupp_muuda.grid(row=2, columnspan=2)
+pealkiri4.grid(row=0, columnspan=2)
+kasutaja_muuda.grid(row=1, column=0)
+texbox_muuda.grid(row=1, column=1)
+pealkiri5.grid(row=2, columnspan=2)
+uus_kasutaja.grid(row=3, column=0)
+uus_texbox.grid(row=3, column=1)
+nupp_muuda.grid(row=4, columnspan=2)
 aken.mainloop()
